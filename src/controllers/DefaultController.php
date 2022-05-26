3aca77d209cf690d22da3b020e90cab96c919b8a
Wojciech Sliwa <wojciechsliwa.st@gmail.com>
2022-05-26 22:42:00 +0200

    Creating routing from lab

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function index() {
+        $this->render('login');
+    }
+
+    public function profile() {
+        $this->render('profile');
+    }
+
+    public function friends() {
+        $this->render('friends');
+    }
+}
