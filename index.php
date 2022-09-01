05f7a096ed2dd9405c1c1187c54e979cd3517c21
Wojciech Sliwa <wojciechsliwa.st@gmail.com>
2022-09-01 20:03:52 +0200

    Auth mock

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url( $path, PHP_URL_PATH);
 
 Routing::get('index', 'DefaultController');
+Routing::post('login', 'SecurityController');
 Routing::get('profile', 'DefaultController');
 Routing::get('friends', 'DefaultController');
 Routing::get('exercises', 'DefaultController');
--- public/views/login.php
+++ public/views/login.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <head>
     <link rel="stylesheet" type="text/css" href="public/css/style.css">
     <title>LOGIN PAGE</title>
@@ -10,10 +11,18 @@
             <img src="public/img/logo.svg">
         </div>
         <div class="login-container">
-            <form>
+            <form class="login" action="login" method="POST">
+                <div class="messages">
+                    <?php if(isset($messages)) {
+                        foreach ($messages as $message) {
+                            echo $messages;
+                        }
+                    }
+                    ?>
+                </div>
                 <input name="email" type="text" placeholder="email@email.com">
                 <input name="password" type="password" placeholder="password">
-                <button>LOGIN</button>
+                <button type="submit">LOGIN</button>
             </form>
         </div>
     </div>
